d091bb4694cecb286844bf27fae3d1e0e0193886
Amauri <amauri.olisboa@gmail.com>
2024-03-09 12:13:19 +0000

    Redirection from fds and pipes done -> need to refactorate and took comments off

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: aconceic <aconceic@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 16:31:48 by aconceic          #+#    #+#             */
-/*   Updated: 2024/03/09 10:59:02 by aconceic         ###   ########.fr       */
+/*   Updated: 2024/03/09 12:11:31 by aconceic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,40 +25,62 @@ int main(int argc, char **argv, char **envp)
 	else
 	{
 		ft_printf("Argumentos e pipe ok;\n");
+		
 		pipex.pid = fork();
+		if (pipex.pid == -1)
+		{
+			ft_printf("error fork() line 29");
+		}
+
 		if (pipex.pid != 0)
 		{
 			//The parent process should be responsible for executing cmd2 and 
 			//reading its input from the pipe. Ensure you close the appropriate
 			//file descriptors (pipefd[1]) before reading from the pipe.
-
-			//need function to
-			//redirect the stdin from the read end of the pipe and
-		 	//the stdout to the output file
-			//output(char **argv[5], char **envp)
-
+			int	output_fd;
+	
+			output_fd = open(argv[4], O_CREAT | O_TRUNC | O_WRONLY, 0755);
+			if (output_fd < 0)
+			{
+				//treat this error descently
+				ft_printf("Error line 45\n");
+			}
+			close(pipex.pipefd[1]); // clse write end of file
 			//Redirect stdin to read from the read end of the pipe (pipefd[0]).
-    		//Redirect stdout to write to the output file.
+			//Redirect stdout to write to the output file.
+			if (dup2(pipex.pipefd[0], STDIN_FILENO) == -1 || dup2(output_fd, STDOUT_FILENO) == -1)
+			{
+				ft_printf("Error dup2 line 35.\n");
+			}
+			close(output_fd);
+			close(pipex.pipefd[0]);
    			//Execute cmd2 using execve.
-			
 		}
 		else
 		{
 			//child - The child process should be responsible for executing cmd1 and 
 			//writing its output to the pipe. Ensure you close the appropriate file 
 			//descriptors (pipefd[0]) after writing to the pipe.
-		
-			//need function to 
-			//redirect the stdin from the input file and
-		 	//the stdout to the write end of the pipe;
-			//construc function input_redirect(char **argv[1], char **envp);
+			int	input_fd;
 
+			input_fd = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY, 0755);
+			if (input_fd < 0)
+			{
+				//treat this error descently
+				ft_printf("Error line 69\n"); 
+			}
+			close(pipex.pipefd[0]); // close read end of file
 			//Redirect stdin to read from the input file.
     		//Redirect stdout to write to the write end of the pipe (pipefd[1]).
+			if (dup2(pipex.pipefd[1], STDOUT_FILENO) == -1 || dup2(input_fd, STDIN_FILENO) == -1)
+			{
+				ft_printf("Error dup2 line 50.\n");
+			}
+			close(input_fd);
+			close(pipex.pipefd[1]);
     		//Execute cmd1 using execve.
 		}
 	}
-	(void)argv;
 	(void)envp;
 	//close() to close the pipe
 	return (0);
